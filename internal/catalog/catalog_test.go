7463200716e0045e23914696dd323bece702439d
Tetsuji Kato <6571039+iwizsophy@users.noreply.github.com>
2026-04-11 21:56:47 +0900

    Format catalog normalization tests

--- internal/catalog/catalog_test.go
+++ internal/catalog/catalog_test.go
@@ -422,9 +422,9 @@ func TestNormalizeHandlesCommonExpressionsWithRealCatalog(t *testing.T) {
 	cases := map[string]string{
 		"GPL-2.0-only WITH Classpath-exception-2.0":   "GPL-2.0",
 		"https://opensource.org/licenses/MIT?ref=abc": "MIT",
-		"EPL-1.0":                                   "EPL-1.0",
-		"CDDL-1.1":                                  "CDDL-1.1",
-		"Functional Source License 1.1":             "Unknown",
+		"EPL-1.0":                       "EPL-1.0",
+		"CDDL-1.1":                      "CDDL-1.1",
+		"Functional Source License 1.1": "Unknown",
 		"Functional Source License 1.1 MIT Future License":  "FSL-1.1-MIT",
 		"Functional Source License 1.1 ALv2 Future License": "FSL-1.1-ALv2",
 	}
